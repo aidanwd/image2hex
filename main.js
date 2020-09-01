393331c28ecdb0133494a84c79c93f29cbab228f
Aidan <30511407+aidanwd@users.noreply.github.com>
2020-09-01 10:11:34 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+    var input = document.getElementById("image");
+    var result = document.getElementById("result");
+    input.addEventListener('change', (e) => {
+        var file = URL.createObjectURL(e.target.files[0]);
+        Jimp.read(file)
+        .then(image => {
+          var final = "";
+          var width = image.bitmap.width;
+          var height = image.bitmap.height;
+          document.getElementById("width").textContent = "Width: " + width;
+          document.getElementById("height").textContent = "Height: " + height;
+          document.getElementById("colorbit").textContent = "Color Bits: " + 24;
+            for(y = 0; y < height; y++) {
+              for(x = 0; x < width; x++) {
+              const {r, g, b, a} = Jimp.intToRGBA(image.getPixelColor(x, y));
+              var hex = rgbToHex(r,g,b);
+              final += hex;
+            }
+          }
+          result.value = final;
+        })
+        .catch(err => {
+          // Handle an exception.
+          console.log(err)
+        });
+      
+      });
+      function rgbToHex(r, g, b) {
+        return "" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+      }
